081016a4eed45053f8ada00ecfcad94b579843f8
Mikepn02 <121401821+Mikepn02@users.noreply.github.com>
2023-08-20 09:41:26 +0200

    ignored build errors

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-
   typescript: {
-    ignoreBuildErrors:true
+    ignoreBuildErrors: true
   },
     experimental: {
       serverActions: true,
       serverComponentsExternalPackages: ["mongoose"],
-    },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
     },
     images: {
       remotePatterns: [
